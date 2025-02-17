d1814e0df1f96f534801fa252b7f1d732e9c968a
Eirlys <100836558+SerenityEirlys@users.noreply.github.com>
2025-02-17 19:25:06 +0700

    Add files via upload

---
+++ data/shop/replies.js
@@ -0,0 +1,30 @@
+// Danh sách các từ khóa và câu trả lời tương ứng
+const replies = {
+    // Thông tin chung
+    'help': ['Xin chào! Tôi có thể giúp gì cho bạn?\nSử dụng các lệnh sau:\n/download - Tải loader\n/buy - Mua sản phẩm\n/support - Liên hệ hỗ trợ'],
+    'menu': ['📋 Command Menu:\n/download - Download loader\n/buy - Purchase product\n/support - Contact support'],
+
+    // Hướng dẫn & Tài liệu
+    'instruct': ['📚 See detailed instructions at: https://docs.arons.wiki/'],
+    'hướng dẫn': ['📚 Bạn có thể xem hướng dẫn tiếng Việt tại: https://docs.arons.wiki/vietnamese'],
+    'docs': ['📖 Documentation: https://docs.arons.wiki/'],
+
+    // Tải loader & TP
+    'download loader': ['⬇️ Use the /download command and select the game you want to download the loader for'],
+    'tải loader': ['⬇️ Sử dụng lệnh /download và chọn game bạn muốn tải'],
+    'file tp': ['🎮 custom tp file: https://discord.com/channels/1104940962804936856/1231627318380859514'],
+
+    // Video & Hướng dẫn
+    'video': ['🎥 Xem video hướng dẫn tại: https://www.youtube.com/@aronslie'],
+    'watch video': ['🎥 Tutorial video: https://www.youtube.com/@aronslie'],
+    'có video': ['🎥 Có! Bạn xem video tại: https://www.youtube.com/@aronslie'],
+
+    // Mua & License
+    'buy': ['💳 Purchase products at: sooon or https://arons.store/'],
+    'mua': ['💳 Mua sản phẩm tại: https://arons.dev/'],
+    'how to get my license': ['📧 If purchased on sellhub: Check Gmail, Spam folder\n🏪 If purchased on arons.store: Check Order'],
+    'how to get key': ['📧 If purchased on sellhub: Check Gmail, Spam folder\n🏪 If purchased on arons.store: Check Order'],
+
+};
+
+export default replies;
